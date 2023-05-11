0d2fbf33cd0cff10ac29a0d16a7bced378f06e7f
Argil <argildevs@gmail.com>
2023-05-11 01:36:53 +0100

    UPDATE

---
+++ js/greetings.js
@@ -0,0 +1,27 @@
+var myDate = new Date();
+    var hrs = myDate.getHours();
+
+    
+
+    var greetingsArray = [
+        'Listening to music constantly',
+        'Automating 5 minute tasks obsessive',
+        'Watching the same shows in repeat',
+        'Resident technology bastard'
+
+    ];
+
+    var randomNumber = Math.floor(Math.random()*greetingsArray.length);
+    var greet= greetingsArray[randomNumber];
+
+    // if (hrs < 12 && hrs >= 4)
+    //     greet = 'Listening to music ';
+    // else if (hrs >= 12 && hrs < 17)
+    //     greet = 'Good Afternoon';
+    // else if (hrs >= 17 && hrs < 20)
+    //     greet = 'Good Evening';
+    // else
+    //     greet = 'Good Night';
+
+    document.getElementById('lblGreetings').innerHTML =
+        '<b>' + greet + '</b>!';
