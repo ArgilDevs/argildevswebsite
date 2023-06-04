8c0c7344c1835d32a6e9431b9892c43ccfeb1881
Argil <argildevs@gmail.com>
2023-06-04 22:01:17 +0100

    even more small fixes

--- js/argil.js
+++ js/argil.js
@@ -16,14 +16,14 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
+  // Display the result in the element with id="countdown"
   document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "OUT ITS OUT RIGHT NOW, LISTEN BELOW:";
+    document.getElementById("countdown").innerHTML = "OUT ITS OUT RIGHT NOW, GO LISTEN";
     document.getElementById("countdown-text").innerHTML = "First Episode is"
   }
-}, 1000);
+}, 500);
